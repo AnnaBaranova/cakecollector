3d6e94469546c10789506d12e5489faaa952d0ce
Anna Baranova <asbaranova1@gmail.com>
2021-06-14 11:32:10 -0400

    add template and url for cake list

--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,24 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Cake:  
+  def __init__(self, name, cal, description):
+    self.name = name
+    self.cal = cal
+    self.description = description
+
+cakes = [
+  Cake ('LemonBomb', 400, 'lemon taste'),
+  Cake ('Macarrone', 350, 'crazy natty'),
+  Cake ('Heaven', 450, 'berries and cheese')
+]
+
+
 def home(request):
   return HttpResponse('<h1>Hello, Cake Lover!</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def cakes_index(request):
+    return render(request, 'cakes/index.html', {'cakes': cakes})
