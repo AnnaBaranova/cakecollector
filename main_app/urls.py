f8c790a311f5a045726b0f7fda0a2f0b285b83c5
Anna Baranova <asbaranova1@gmail.com>
2021-06-16 22:25:22 -0400

    add detail page, admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
      path('about/', views.about, name='about'),
      path('cakes/', views.cakes_index, name='index'),
+     path('cakes/<int:cake_id>/', views.cake_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cake
 
 # Create your views here.
 
@@ -6,10 +7,15 @@ from django.http import HttpResponse
 
 
 def home(request):
-  return HttpResponse('<h1>Hello, Cake Lover!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def cakes_index(request):
-    return render(request, 'cakes/index.html', {'cakes': cakes})
+  cakes = Cake.objects.all()
+  return render(request, 'cakes/index.html', {'cakes': cakes})
+
+def cake_detail(request, cake_id):
+  cake = Cake.objects.get(id=cake_id)
+  return render(request, 'cakes/detail.html', { 'cake': cake })
